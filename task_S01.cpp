223c3f5272b4047dfa1708bd98fc9504941b84fd
MihailValiulin <152514805+MihailValiulin@users.noreply.github.com>
2023-12-12 22:26:57 +0800

    task_S01.cpp

--- task_S01.cpp
+++ task_S01.cpp
@@ -1,1 +1,40 @@
 
+#include <stdio.h>
+
+int main()
+{
+//целое 
+    int a;
+    a = 15;
+    printf("a: %d\n", a);
+    
+    //с плавающей точкой
+    float b;
+    b = 20;
+    printf("b: %f\n", b);
+    
+    //символ
+    char c;
+    c = 92;
+    printf("c: %c\n", c);
+    
+    //Булева переменная
+    bool d;
+    d = 0;
+    printf("d: %d\n", d);
+    
+    //операторы
+    int number = 15;
+    number += 26;
+    printf (" %d \n", number);
+    number -= 6;
+    printf (" %d \n", number);
+    number *= 5;
+    printf (" %d \n", number);
+    float n;
+    n = 48;
+    n /= 10;
+    printf (" %f \n", n);
+        
+    return 0;
+    }
