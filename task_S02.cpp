f4cea4d68de2987e0f4a352de187e9652dd96c3d
MihailValiulin <152514805+MihailValiulin@users.noreply.github.com>
2023-12-13 00:29:12 +0800

    task_S12.cpp

--- task_S02.cpp
+++ task_S02.cpp
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main()
+{
+    int s=0, a=1;
+  
+    while (a<=1000){
+        if (a%7==0)
+        s=s+a;
+        a=a+4;;
+        }
+    printf("%d", s);
+    return 0;
+}
